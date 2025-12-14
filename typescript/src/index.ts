d7a87c927e0015719f90feb54fdfa27fe9612619
Shivam Kumar <shivam108ku@gmail.com>
2025-12-14 18:54:20 +0530

    add tuple and object type definitions in index.ts

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -15,4 +15,28 @@ let array : number[] = [1 ,2 ,3 ];
 let array2 : any[] = [1,2,3,4, "false"];
 
 let data : (number | string)[] =[1 , 3 , 4 , 67, "dfgdf"]; 
+// tuple
+ let data3 : [number , string , boolean]  = [1 ,"rtrt" ,false ]; 
 console.log(array2);
+
+// Types for object 
+// clases
+// interface
+
+interface Product {
+    name :string;
+    price :number;
+    display() :void;
+}
+
+let p1 : Product = {
+    name : "Shivam",
+    price : 100000000,
+    display: ()=>{
+        console.log("first")
+    }
+}
+
+console.log(p1);
+
+
