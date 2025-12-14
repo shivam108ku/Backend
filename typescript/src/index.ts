aba9ae2034ea5b9297bd8ee2a46f23265203f488
Shivam Kumar <shivam108ku@gmail.com>
2025-12-14 18:33:49 +0530

    add initial variables and data structures in index.ts

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,2 +1,18 @@
 let id : number = 5;
 console.log(id);
+
+let firstName = "Shivam";
+var lastName = "Kumar";
+
+// Union 
+let userid : number | string = "26";
+
+//  array 
+
+let array : number[] = [1 ,2 ,3 ];
+
+// hetrogenous array 
+let array2 : any[] = [1,2,3,4, "false"];
+
+let data : (number | string)[] =[1 , 3 , 4 , 67, "dfgdf"]; 
+console.log(array2);
