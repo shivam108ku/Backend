2660c3d27eee98352dc2a6f1a011697eeb578688
Shivam Kumar <shivam108ku@gmail.com>
2025-12-18 16:06:09 +0530

    add: implement chai menu, pricing, and tuple definitions in arraysTupEnum.ts

---
+++ typescript/src/arraysTupEnum.ts
@@ -0,0 +1,30 @@
+const chaiFlavours: string[] = ["Masala" , "Adrak"];
+const pricing : number[] = [10 , 20];
+
+// second way to define array 
+const rating: Array<number> = [4.5];
+
+type Chai = {
+    name: string;
+    price: number
+}
+
+const menu: Chai[] = [
+    {name: 'Masala' , price: 15},
+    {name: 'Masalas' , price: 25},
+]
+
+// after adding the radonly property we can not modifiy this array
+const cities: readonly string[] = ["Delhi" ,"Varanasi"];
+
+// 2d array
+const table: number[][] = [
+    [1,2,3,4],
+    [4,5,6]
+]
+
+// Tuples
+
+let chaiTuple : [string, number];
+chaiTuple = ["Masala", 20]; // isme order bhi dekhna hoga aur aati array me hi h 
+
