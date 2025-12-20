af1731d7212a524c8e92e889c4050777cc90c064
Shivam Kumar <shivam108ku@gmail.com>
2025-12-20 16:54:17 +0530

    adding config for env in good manner

--- ExressServer/src/config/index.ts
+++ ExressServer/src/config/index.ts
@@ -1,9 +1,13 @@
 import dotenv from 'dotenv';
+type ServerConfig = {
+    PORT: number
+}
 
 function loadEnv(){
     dotenv.config();
 }
+loadEnv();
 
-export default loadEnv;
-
-
+export const serverConfig: ServerConfig = {
+    PORT: Number(process.env.PORT) || 3001  
+}
--- ExressServer/src/server.ts
+++ ExressServer/src/server.ts
@@ -1,14 +1,13 @@
 import express from 'express';
-import loadEnv from './config';
+import { serverConfig} from './config';
 const app = express();
 
-loadEnv();
 console.log("Env Loaded");
 
 app.get('/ping', (req , res)=>{
     res.send('Pong');
 });
 
-app.listen(process.env.PORT, ()=>{
-    console.log(`Server is running on the ${process.env.PORT}`);
+app.listen(serverConfig.PORT, ()=>{
+    console.log(`Server is running on the ${serverConfig.PORT}`);
 });
