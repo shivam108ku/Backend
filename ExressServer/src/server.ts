1a54aa9acd9eadcc426d465b59c9771192728b66
Shivam Kumar <shivam108ku@gmail.com>
2025-12-21 18:51:02 +0530

    comment

--- ExressServer/src/server.ts
+++ ExressServer/src/server.ts
@@ -6,6 +6,7 @@ const app = express();
 console.log("Env Loaded");
 app.use(pingRouter)
 
+// these are the main way to make this 
 
 app.listen(serverConfig.PORT, ()=>{
     console.log(`Server is running on the ${serverConfig.PORT}`);
